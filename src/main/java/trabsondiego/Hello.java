df203d28b05f0698e2958d4b9950310a3e509e11
hiderjares <hider1195@gmail.com>
2018-10-23 12:30:20 -0300

    l

---
+++ src/main/java/trabsondiego/Hello.java
@@ -0,0 +1,8 @@
+package trabsondiego;
+
+public class Hello {
+    @SuppressWarnings("unused")
+    private static void main(final String[] args) {
+        System.out.println(" da-lhe negada ");
+    }
+}
